dfb01e46627e916a63fc915a94facc2518c1b77c
Bryym <1510cyan@gmail.com>
2023-04-18 09:46:03 +0800

    new carpool

--- regprocess.php
+++ regprocess.php
@@ -40,11 +40,11 @@ if (isset($_POST['reg']) && $_POST['user_type'] == "Passenger" || $_POST['user_t
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                        //Enable verbose debug output
             $mail->isSMTP();                                              //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                         //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                     //Enable SMTP authentication
+            $mail->SMTPAuth   = 'true';                                     //Enable SMTP authentication
             $mail->Username   = '1510cyan@gmail.com';                     //SMTP username - fuerteramonchristopher@gmail.com
             $mail->Password   = 'cpxgbytsouoxdwij';                       //SMTP password - biehdlavioyicybc
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;              //Enable implicit TLS encryption
-            $mail->Port       = 465;                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->Port       = '465';                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
             $mail->setFrom('1510cyan@gmail.com', 'User Registration');
@@ -55,7 +55,7 @@ if (isset($_POST['reg']) && $_POST['user_type'] == "Passenger" || $_POST['user_t
             $mail->Subject = "Your verify code";
             $mail->Body    = "<h2><b> Carpool App </b></h2> <br> <p>Dear $fname, </p> <br> <h3>Good day, you only have one step to use the app. 
     Click the link below to finalize the Carpool App Registration<br></h3>
-    <p><a href='http://localhost/carpool/confirmacc.php?email=$email&v_code=$v_code'>Verifying Email Address</a></p>
+    <p><a href='https://bryy.tech/confirmacc.php?email=$email&v_code=$v_code'>Verifying Email Address</a></p>
     <br><br>
     <p>With regards,</p>
     <b>Carpooling App</p>";
