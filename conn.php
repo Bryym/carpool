b48a298281975bc33b47e805ecb76a5fb40953a1
Bryym <1510cyan@gmail.com>
2023-04-18 09:58:06 +0800

    new updated

--- conn.php
+++ conn.php
@@ -1,8 +1,13 @@
 <?php
-$sname= "localhost";
-$user= "u235219407_bryan";
-$password = "Bryan@15";
-$database = "u235219407_carpool_bryan";
+// $sname= "localhost";
+// $user= "u235219407_bryan";
+// $password = "Bryan@15";
+// $database = "u235219407_carpool_bryan";
 
+$sname= "localhost";
+$user= "root";
+$password = "";
+$database = "carpool";
 $conn = mysqli_connect($sname, $user, $password, $database);
 ?>
+
