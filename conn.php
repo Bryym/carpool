6a5a39e95758663d888d9d2da6505a425981217c
Bryym <1510cyan@gmail.com>
2023-04-18 09:37:59 +0800

    updated carpool

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
 $sname= "localhost";
-$user= "root";
-$password = "";
-$database = "carpool";
+$user= "u235219407_bryan";
+$password = "Bryan@15";
+$database = "u235219407_carpool_bryan";
 
 $conn = mysqli_connect($sname, $user, $password, $database);
 ?>
