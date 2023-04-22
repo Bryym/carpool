6905f60f64eadfc1e98422c4d4ee3a0641d37427
Bryym <1510cyan@gmail.com>
2023-04-22 20:04:15 +0800

    sdaw

--- landing.php
+++ landing.php
@@ -1,5 +1,16 @@
 <?php
-include "conn.php";
+$sname= "localhost";
+$user= "u235219407_bryan";
+$password = "Bryan@15";
+$database = "u235219407_carpool_bryan";
+
+// $sname= "localhost";
+// $user= "root";
+// $password = "";
+// $database = "carpool";
+
+
+$conn = mysqli_connect($sname, $user, $password, $database);
 ?>
 <!DOCTYPE html>
 <html lang="en">
